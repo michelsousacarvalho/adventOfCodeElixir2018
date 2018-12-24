5336395d253d35c92395f14cb5e73ccba41ed5e0
Michel Sousa <michelsousacarvalho@gmail.com>
2018-12-24 01:16:21 -0200

    Performance using Task

--- Day1/day1_part2Task.exs
+++ Day1/day1_part2Task.exs
@@ -1,19 +1,25 @@
 defmodule Day1 do 
 	def repeated_frequency(file_stream) do 
-		file_stream 
-		|> Stream.map(fn line ->
-			{integer, _leftover} = Integer.parse(line)
-			integer 
-		end)
-		|> Stream.cycle()
-		|> Enum.reduce_while({0, MapSet.new([0])}, fn x, {current_frequency, seen_frequencies} -> 
-			new_frequency = current_frequency + x
-			if new_frequency in seen_frequencies do
-				{:halt, new_frequency}
-			else
-				{:cont, {new_frequency, MapSet.put(seen_frequencies, new_frequency)}}
-		end
-		end)
+		Task.async(fn -> 
+	 		Process.put({__MODULE__,0}, true)
+		
+			file_stream 
+			|> Stream.map(fn line ->
+				{integer, _leftover} = Integer.parse(line)
+				integer 
+			end)
+			|> Stream.cycle()
+			|> Enum.reduce_while(0, fn x, current_frequency -> 
+			 	new_frequency = current_frequency + x
+				key = {__MODULE__, new_frequency}
+				if Process.get(key) do
+					{:halt, new_frequency}
+				else 
+					Process.put({__MODULE__, new_frequency}, true)
+					{:cont, new_frequency}
+				end
+			end)
+		end) Task.await(:infinity)
 	end
 end
 
